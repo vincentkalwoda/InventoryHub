7bd6b61b01dac0fc163691116188a152b96a75e5
Vincent Kalwoda <92054282+vincentkalwoda@users.noreply.github.com>
2024-11-07 15:03:23 +0100

    Reapply "Created SpringBoot Project & PUML"
    This reverts commit 393b84bd33c31e0f69d09aac4b5d5ed2a7a7b7b2.

---
+++ MovieManagaement/src/main/java/com/example/moviemanagaement/MovieManagaementApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviemanagaement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagaementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieManagaementApplication.class, args);
+    }
+
+}
