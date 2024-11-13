bb08be255cc6695c86b9436552251705143177c6
Vincent Kalwoda <92054282+vincentkalwoda@users.noreply.github.com>
2024-11-13 21:21:14 +0100

    Added EmailConverter + Tests

---
+++ InventoryHub/src/test/java/at/kalwodaknezevic/inventoryhub/domain/EmailTest.java
@@ -0,0 +1,25 @@
+package at.kalwodaknezevic.inventoryhub.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void when_instantiated_with_null_throws_appropriate_EmailException() {
+        Email.EmailException ex = assertThrows(Email.EmailException.class, () -> new Email(null));
+        assertEquals("Email value must not be null!", ex.getMessage());
+    }
+
+    @Test
+    void when_instantiated_with_invalid_value_throws_appropriate_EmailException() {
+        Email.EmailException ex = assertThrows(Email.EmailException.class, () -> new Email("invalid"));
+        assertEquals("Email value is invalid!", ex.getMessage());
+    }
+
+    @Test
+    void when_instantiated_with_valid_value_does_not_throw_exception() {
+        assertDoesNotThrow(() -> new Email("kal210629@spengergasse.at"));
+    }
+}
