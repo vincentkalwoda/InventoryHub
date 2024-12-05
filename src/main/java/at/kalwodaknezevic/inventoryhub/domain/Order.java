47a7d24e7b7cb827a5f0ef1bfc18a9e653613790
ZaDelfy <KNE210630@spengergasse.at>
2024-12-05 14:40:46 +0100

    Order Repo tests, updated Order, deleted useless files

--- src/main/java/at/kalwodaknezevic/inventoryhub/domain/Order.java
+++ src/main/java/at/kalwodaknezevic/inventoryhub/domain/Order.java
@@ -1,19 +1,27 @@
 package at.kalwodaknezevic.inventoryhub.domain;
 
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 
+@Entity
+@Table(name = "orders")
 public class Order {
     @EmbeddedId
     private OrderId orderId;
-    @NotNull
+    @OneToMany
     private List<Article> orderItems;
-    @NotNull
+    @ManyToOne
     private Supplier supplier;
     @NotNull
     private LocalDate orderDate;
